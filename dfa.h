477d35e34ac55f8b6720652d5fa5e9bb9ad1f042
Domenic R <domenic_ruocco@student.uml.edu>
2019-10-10 12:23:15 -0400

    Completed Task # 11, 12 and 13
    Finally completed these tasks, 11 wasnt hard, neither was 13, but 12 was absolutely the hardest. probbaly spend a few hours trying to make an algorithm that returned a string that was accepted

--- dfa.h
+++ dfa.h
@@ -2,7 +2,8 @@
 #define DFA_H
 
 #include <iostream>
-#include <vector> 
+#include <vector>
+#include <algorithm>
 #include "char.h"
 
 using namespace std;
@@ -13,6 +14,12 @@ class DFA{
         DFA(vector<Char>, vector<Char>, Char, vector<Char>, vector< vector<Char> >);
         //Run the DFA with a string
         bool eval(vector<Char>);
+        //Run the DFA and return strings of nodes visited
+        vector<Char> eval_trace(vector<Char>);
+        //Return an accepted string
+        vector<Char> find_accepted(void);
+        //Returns the compliment
+        DFA invert(void);
     private:
         vector<Char> Alpha;
         vector<Char> Nodes;
@@ -105,5 +112,187 @@ DFA onlyChar(Char c){
 bool test_dfa(DFA d, vector<Char> list){
     return d.eval(list);
 }
+
+vector<Char> DFA::eval_trace(vector<Char> list){
+
+    vector<Char> Trace;
+
+    //Check if empty string is given and in accepted
+    if(list.size() == 0 || (list.size() == 1 && list.at(0).getChar() == 'e')){
+        for(int i=0;i<Accepting.size();i++){
+            if(Start.getChar() == Accepting.at(i).getChar()){
+                Trace.push_back(Start);
+                return Trace;
+            }
+        }
+        return Trace;
+    }
+
+    Char currNode = Start;
+    Trace.push_back(Start);
+
+    while(list.size()>0){
+        
+        //Grab row index
+        int indexString = -1;
+        for(int i=0;i<Alpha.size();i++){
+            if(list.at(0).getChar() == Alpha.at(i).getChar()){
+                indexString = i;
+            }
+        }
+
+        //Grab column index
+        int indexNode = -1;
+        for(int i=0;i<Nodes.size();i++){
+            if(currNode.getChar() == Nodes.at(i).getChar()){
+                indexNode = i;
+            }
+        }
+
+        //If Delta does not contain the next step return trace already done
+        if(indexString == -1 || indexNode == -1){ return Trace; }
+        if(indexString >= Delta.size() || indexNode >= Delta.at(indexString).size()){ return Trace; }
+
+        //node = delta[row][column]
+        currNode = Delta.at(indexString).at(indexNode);
+        //Remove the read term
+        list.erase(list.begin());
+        //Add to trace
+        Trace.push_back(currNode);
+
+    }
+
+    //If nothing worked, return false
+    return Trace;
+}
+
+//Will return accepted string or an empty string if nothing is accepted
+//If an empty string would normally be accepted, will return a string with just epsilon
+vector<Char> DFA::find_accepted(void){
+    // Alpha, Nodes, Start, Accepting, Delta
+
+    vector<Char> Accept;
+
+    //Check if accepting nodes has any node
+    if(Accepting.size() == 0 ){ return Accept; } 
+
+    //Check if accepting is a node
+    int accept_exists = 0;
+    for(int i=0;i<Nodes.size();i++){
+        for(int j=0;j<Accepting.size();j++){
+            if(Nodes.at(i).getChar() == Accepting.at(j).getChar()){
+                accept_exists = 1;
+            }
+        }
+    }
+
+    //If accepting isnt a node, you cannot go to it
+    if(accept_exists == 0){ return Accept; }
+
+    //If the start node is accepting, just return epsilon
+    for(int i=0;i<Accepting.size();i++){
+        if(Start.getChar() == Accepting.at(i).getChar()){
+            Accept.push_back('e');
+            return Accept;
+        }
+    }
+
+    vector<Char> Visited;
+    Visited.push_back(Start);
+
+    while(true){        
+        //for all visited,
+        //find its index in alpha,
+        //for all columns in Delta, add delta[all][index] if it is not already in visited
+        //Check if any nodes are in accepted
+        //if they are, find a way to get from start to that node
+
+        //Check if any visited nodes are accepted
+        for(int i=0;i<Visited.size();i++){
+            for(int j=0;j<Accepting.size();j++){
+                if(Visited.at(i).getChar() == Accepting.at(j).getChar()){
+                    
+                    Char target = Visited.at(i);
+                    
+                    while(true){                    
+
+                        for(int x=0;x<Delta.size();x++){
+                            for(int y=0;y<Delta.at(x).size();y++){
+                                if(Delta.at(x).at(y).getChar() == target.getChar()){
+                                    target = Nodes.at(y);
+                                    Accept.push_back(Alpha.at(x));
+                                    
+                                    if(target.getChar() == Start.getChar()){
+                                        reverse(Accept.begin(), Accept.end());
+                                        return Accept;
+                                    }
+                                }
+                            }
+                        }
+
+                    }
+
+                }
+            }
+        }
+        
+
+
+            //Loop on all visited
+        for(int i=0;i<Visited.size();i++){
+            //Find Visited.at(i)'s index within the Alpha
+            int index = -1;
+            for(int j=0;j<Nodes.size();j++){
+                if(Visited.at(i).getChar() == Nodes.at(j).getChar()){
+                    index = j;
+                }
+            }
+
+            //Loop all columns of Delta
+            for(int j=0;j<Delta.size();j++){
+                if(Delta.at(j).size() > index){
+                    Char Next = Delta.at(j).at(index);
+                    int found_in_vis = 0;
+
+                    //Find out if Next is in Visited
+                    for(int k=0;k<Visited.size();k++){
+                        if(Visited.at(k).getChar() == Next.getChar()){
+                            found_in_vis = 1;
+                        }
+                    }
+                    
+                    //If not found, add to Visited
+                    if(found_in_vis == 0){
+                        Visited.push_back(Next);
+                    }
+                }
+            }
+        }
+    }
+    //Just in case
+    return {'e'};
+}
+
+DFA DFA::invert(void){
+
+    vector<Char> newAccepting;
+
+    int new_a = 0;
+
+    for(int i=0;i<Nodes.size();i++){
+        for(int j=0;j<Accepting.size();j++){
+            if(Nodes.at(i).getChar() == Accepting.at(j).getChar()){
+                new_a = 1;
+            }
+        }
+        if(new_a == 0){
+            newAccepting.push_back(Nodes.at(i));
+        }
+    }
+
+    return {Alpha, Nodes, Start, newAccepting, Delta};
+
+}
+
 
 #endif
--- main.cpp
+++ main.cpp
@@ -8,7 +8,25 @@ using namespace std;
 
 int main(int argc, char* argv[]){
 
-    Do_TESTS();
+    //Odd Binary Number DFA
+    DFA d(
+        {'0', '1'},  //Alphabet
+        {'A', 'B'},  //Nodes
+        'A',         //Start
+        {'B'},       //Accepting
+        { {'A', 'A'}, {'B', 'B'} }  //Delta
+    );
+    vector<Char> list;
+
+    list = d.find_accepted();
+
+    printListChar(list);
+
+    d = d.invert();
+
+    list = d.find_accepted();
+
+    printListChar(list);
 
     return 0;
 }
