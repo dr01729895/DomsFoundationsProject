7e008c31b022f08d41910fdf45caa26fff3d3140
Domenic R <domenic_ruocco@student.uml.edu>
2019-10-16 20:37:12 -0400

    Added 3 exmaple NFAs
    I added 3 example NFAs, a third from last is 1, a third from last is 0, and a finaly one that checks if the string contains a 010 within it

--- main.cpp
+++ main.cpp
@@ -9,7 +9,82 @@ using namespace std;
 
 int main(int argc, char* argv[]){
 
-    cout << "Hello, world" << endl;
+    //Alpha, Nodes, Start, Accepting, Delta Function
+    //Delta = [Column 1  Column 2 ... Column n]
+    //Column = [possible nodes]
+    NFA third_from_last_one(
+        //Alphabet
+        {'0','1'},
+        //List of Nodes
+        {'A','B','C','D'},
+        //Starting Node
+        'A',
+        //Accepting Nodes
+        {'D'},
+        //Delta Function
+        {
+            //Transition if first alphabet char is found 
+            {
+                //a0=a, b0=c, c0=d, d0=a 
+                {'A'}, {'C'}, {'D'}, {'A'}
+            },
+            //Transition if second alphabet char is found 
+            { 
+                //a1=a or b, b1=c, c1=d, d1=a
+                {'A','B'}, {'C'}, {'D'}, {'A'} 
+            } 
+        }
+    );
+
+    NFA third_from_last_zero(
+        //Alphabet
+        {'0','1'},
+        //List of Nodes
+        {'A','B','C','D'},
+        //Starting Node
+        'A',
+        //Accepting Nodes
+        {'D'},
+        //Delta Function
+        {
+            //Transition if first alphabet char is found 
+            {
+                //a0=a or b, b0=c, c0=d, d0=a 
+                {'A','B'}, {'C'}, {'D'}, {'A'}
+            },
+            //Transition if second alphabet char is found 
+            { 
+                //a1=a, b1=c, c1=d, d1=a
+                {'A'}, {'C'}, {'D'}, {'A'} 
+            } 
+        }
+    );
+
+    NFA contains_zero_one_zero(
+        //Alphabet
+        {'0','1'},
+        //List of Nodes
+        {'A','B','C','D'},
+        //Starting Node
+        'A',
+        //Accepting Nodes
+        {'D'},
+        //Delta Function
+        {
+            //Transition if first alphabet char is found 
+            {
+                //a0=a or b, b0=a, c0=d, d0=d 
+                {'A','B'}, {'A'}, {'D'}, {'D'}
+            },
+            //Transition if second alphabet char is found 
+            { 
+                //a1=a, b1=c, c1=a, d1=d
+                {'A'}, {'C'}, {'A'}, {'D'} 
+            } 
+        }
+    );
+
+    cout << "Worked" << endl;
 
     return 0;
 }
